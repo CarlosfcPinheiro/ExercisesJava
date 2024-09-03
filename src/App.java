099bd5ffd99235115116f6736f2d73e2851d177a
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2024-09-03 14:42:44 -0300

    enviar arquivos iniciais

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args){
+        String rg = new String("1234-789");
+        String name = new String("Afonso Berinaldo Silva");
+        // new => is used to refer an instaciating action, in case, about the String class
+        // Or => String name = "Afonso Berinaldo Silva"
+
+        System.out.println("Name: " + name);
+        System.out.println("RG: " + rg);
+    }
+}
